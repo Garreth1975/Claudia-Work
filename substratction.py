4e60cda3333b5d14c0d3a1f833bc13b8fd3d60a7
Jeff Destine <jdestine@walmart.com>
2018-11-18 18:44:54 -0500

    all

---
+++ substratction.py
@@ -0,0 +1,19 @@
+l = ["Claudia", "Jeff", "Gilbert", "Garreth"]
+m = ("Claudia", "Jeff", "Gilbert", "Garreth")
+
+type(l)
+type(m)
+print(m)
+print(len(m))
+print(l)
+print(len(l))
+l.append("Jenelle")
+l.insert(0, "Jeanice")
+
+def add(n):
+  for i in l:
+    l.append(n)
+print(add)
+
+add("Joe")
+print(add)
